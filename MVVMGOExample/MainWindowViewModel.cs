012d1e660b49c074c59b9d2856991a6d4d574a47
Ali Yousefi <ali.visual.studio@gmail.com>
2018-05-12 16:15:03 +0430

    first initialize

---
+++ MVVMGOExample/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MvvmGo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMGOExample
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public MainWindowViewModel()
+        {
+            TestItems = new List<string>() { "ali", "Yousefi", "Telori" };
+        }
+
+        int _CurrentStatus;
+        public int CurrentStatus
+        {
+            get
+            {
+                return _CurrentStatus;
+            }
+            set
+            {
+                _CurrentStatus = value;
+                OnPropertyChanged(nameof(CurrentStatus));
+            }
+        }
+        public List<string> TestItems { get; set; }
+
+    }
+}
